e145b638ab7b644a0123d50ff048e142694e8346
Perc1v4l <sansan17volodin@mail.ru>
2023-03-30 16:19:35 +0300

    add methods

--- src/main/java/org/example/WorkTimeWorker.java
+++ src/main/java/org/example/WorkTimeWorker.java
@@ -6,6 +6,7 @@ import com.taskadapter.redmineapi.TimeEntryManager;
 import com.taskadapter.redmineapi.bean.TimeEntry;
 import com.taskadapter.redmineapi.bean.User;
 
+import org.javatuples.Pair;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -32,6 +33,7 @@ public class WorkTimeWorker {
             userChoice = scanner.nextInt();
             switch (userChoice) {
                 case 1 -> task1(mgr);
+                case 2 -> task2(mgr);
                 case 0 -> System.out.println("Good Bye");
             }
         } while (userChoice != 0);
@@ -52,7 +54,7 @@ public class WorkTimeWorker {
 
         final List<User> users = mgr.getUserManager().getUsers();
 
-        System.out.println("Select the user whose work time you want to see");
+        System.out.println("Выберите сотрудника");
         users.forEach(user -> System.out.println(user.getId() + ") " + user.getFullName()));
 
         params.put("user_id", scanner.next());
@@ -82,9 +84,9 @@ public class WorkTimeWorker {
         mapset.forEach(System.out::println);
 
         System.out.println("Дни, в которые сотрудник работал менее 8 часов");
-        for(Date date = startDate; date.equals(endDate); date = DateUtil.addDays(date, 1)) {
-            if(dateHoursMap.containsKey(date)) {
-                if(dateHoursMap.get(date) < 8) {
+        for (Date date = startDate; date.equals(endDate); date = DateUtil.addDays(date, 1)) {
+            if (dateHoursMap.containsKey(date)) {
+                if (dateHoursMap.get(date) < 8) {
                     System.out.println("Дата: " + date + "\tКол-во часов: " + dateHoursMap.get(date));
                 }
             } else {
@@ -93,10 +95,8 @@ public class WorkTimeWorker {
         }
     }
 
-    public static class DateUtil
-    {
-        public static Date addDays(Date date, int days)
-        {
+    public static class DateUtil {
+        public static Date addDays(Date date, int days) {
             Calendar cal = Calendar.getInstance();
             cal.setTime(date);
             cal.add(Calendar.DATE, days);
@@ -105,21 +105,28 @@ public class WorkTimeWorker {
     }
 
     public void task2(RedmineManager mgr) throws RedmineException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.dd.yyyy");
-
         TimeEntryManager timeEntryManager = mgr.getTimeEntryManager();
 
         final Map<String, String> params = new HashMap<>();
 
-        System.out.println("Введите начальную дату");
-        Date startDate = java.sql.Date.valueOf(LocalDate.parse(scanner.next(), formatter));
+        System.out.print("Введите месяц: ");
+        int month = scanner.nextInt();
 
-        System.out.println("Введите конечную дату");
-        Date endDate = java.sql.Date.valueOf(LocalDate.parse(scanner.next(), formatter));
+        System.out.print("Введите год: ");
+        int year = scanner.nextInt();
 
         final List<User> users = mgr.getUserManager().getUsers();
-        System.out.println("Select the user whose work time you want to see");
+        System.out.println("Выберите сотрудника");
         users.forEach(user -> System.out.println(user.getId() + ") " + user.getFullName()));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, 1);
+
+        Date startDate = calendar.getTime();
+
+        calendar.set(year, month, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        Date endDate = calendar.getTime();
 
         params.put("user_id", scanner.next());
         params.put("from", startDate.toString());
@@ -129,5 +136,113 @@ public class WorkTimeWorker {
         elements = elements.stream()
                 .sorted(Comparator.comparing(TimeEntry::getSpentOn))
                 .toList();
+
+        float sumOfHoursInWeek = 0;
+        calendar.set(year, month, 1);
+        int weekInMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+
+        List<Float> hoursInWeek = new ArrayList<>();
+
+        for (TimeEntry element : elements) {
+            calendar.setTime(element.getSpentOn());
+
+            if (calendar.get(Calendar.WEEK_OF_MONTH) == weekInMonth) {
+                sumOfHoursInWeek += element.getHours();
+            } else {
+                hoursInWeek.add(sumOfHoursInWeek);
+                sumOfHoursInWeek = element.getHours();
+                weekInMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+            }
+        }
+
+        System.out.println("Рабочие часы сотрудника по неделям");
+        hoursInWeek.forEach(el -> System.out.println("Неделя " + hoursInWeek.indexOf(el) + ": " + el));
+
+        System.out.println("Недели, в которых рабочее время сотрудника составило менее 40 часов");
+        hoursInWeek.forEach(el ->
+        {
+            if (el < 40) {
+                System.out.println("Неделя " + hoursInWeek.indexOf(el) + ": " + el);
+            }
+        });
+    }
+
+    public void task3(RedmineManager mgr) throws RedmineException {
+        float[] weeks = {
+                0,
+                0, 40, 40, 40,
+                40, 40, 40, 23, 40,
+                31, 40, 40, 40,
+                40, 40, 40, 40,
+                32, 24, 40, 40, 40,
+                40, 32, 40, 40,
+                40, 40, 40, 40, 40,
+                40, 40, 40, 40,
+                40, 40, 40, 40,
+                40, 40, 40, 40, 39,
+                32, 40, 40, 40,
+                40, 40, 40, 40
+        };
+
+        TimeEntryManager timeEntryManager = mgr.getTimeEntryManager();
+
+        final Map<String, String> params = new HashMap<>();
+
+        System.out.print("Введите месяц: ");
+        int month = scanner.nextInt();
+
+        System.out.print("Введите год: ");
+        int year = scanner.nextInt();
+
+        final List<User> users = mgr.getUserManager().getUsers();
+        System.out.println("Выберите сотрудника");
+        users.forEach(user -> System.out.println(user.getId() + ") " + user.getFullName()));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, 1);
+
+        Date startDate = calendar.getTime();
+
+        calendar.set(year, month, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        Date endDate = calendar.getTime();
+
+        params.put("user_id", scanner.next());
+        params.put("from", startDate.toString());
+        params.put("to", endDate.toString());
+
+        List<TimeEntry> elements = timeEntryManager.getTimeEntries(params).getResults();
+        elements = elements.stream()
+                .sorted(Comparator.comparing(TimeEntry::getSpentOn))
+                .toList();
+
+        float sumOfHoursInWeek = 0;
+        calendar.set(year, month, 1);
+        int weekInMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+
+        List<Pair<Integer, Float>> hoursInWeek = new ArrayList<>();
+
+        for (TimeEntry element : elements) {
+            calendar.setTime(element.getSpentOn());
+
+            if (calendar.get(Calendar.WEEK_OF_MONTH) == weekInMonth) {
+                sumOfHoursInWeek += element.getHours();
+            } else {
+                hoursInWeek.add(new Pair(weekInMonth, sumOfHoursInWeek));
+                sumOfHoursInWeek = element.getHours();
+                weekInMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+            }
+        }
+
+        System.out.println("Рабочие часы сотрудника по неделям");
+        hoursInWeek.forEach(el -> System.out.println("Неделя " + el.getValue0() + ": " + el.getValue1()));
+
+        System.out.println("Недели, в которых рабочее время сотрудника составило менее 40 часов");
+        hoursInWeek.forEach(el ->
+        {
+            if (el.getValue1() < weeks[el.getValue0()]) {
+                System.out.println("Неделя " + hoursInWeek.indexOf(el) + ": " + el);
+            }
+        });
     }
 }
